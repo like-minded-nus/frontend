ec1fb01dbc2d8a4b89339d17d909859f07834381
Loh Chun Mun <102757707+cmsamaaa@users.noreply.github.com>
2024-04-03 01:26:43 +0800

    Revert "add redux persist"
    This reverts commit 70eedd78bad7ed431bd383772845cb43af713010.

--- redux/provider.tsx
+++ redux/provider.tsx
@@ -1,13 +1,8 @@
 'use client';
 
-import { store, persistor } from './store';
+import { store } from './store';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 
 export const ReduxProvider = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>{children}</PersistGate>
-    </Provider>
-  );
+  return <Provider store={store}>{children}</Provider>;
 };
--- redux/store.ts
+++ redux/store.ts
@@ -3,35 +3,15 @@ import menuReducer from './features/menuSlice';
 import userReducer from './features/userSlice';
 import matchReducer from './features/matchSlice';
 import profileReducer from './features/profileSlice';
-import { combineReducers } from 'redux';
-import storage from 'redux-persist/lib/storage';
-import { persistReducer, persistStore } from 'redux-persist';
-
-const persistConfig = {
-  key: 'root',
-  storage,
-  blacklist: ['menuReducer', 'userReducer', 'matchReducer'],
-  // whitelist: ['profileReducer'],
-};
-
-const rootReducer = combineReducers({
-  menuReducer,
-  userReducer,
-  matchReducer,
-  profileReducer,
-});
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }),
+  reducer: {
+    menuReducer,
+    userReducer,
+    matchReducer,
+    profileReducer,
+  },
 });
-
-export let persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
